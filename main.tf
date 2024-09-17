c731e3f4fad1906c27fd50779ab739071bb82878
rahul-practice-account <rahuldabhade1254@gmail.com>
2024-09-17 21:45:40 +0530

    Resolve the provider issue

--- main.tf
+++ main.tf
@@ -1,13 +1,16 @@
+# Configure the AWS provider
 provider "aws" {
-  region                   = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "my-us-east-1-instance" {
-  ami           = "ami-066784287e358dad1"
+# Create a new EC2 instance
+resource "aws_instance" "my-ec2-instance" {
+  ami           = "ami-0e86e20dae9224db8" 
   instance_type = "t2.nano"
   availability_zone = "us-east-1a"
 
+  # Add tags to the instance
   tags = {
-    Name = "Rahul east-1 ec1 with zone"
+    Name = "My EC2 Instance"
   }
 }
