0bb74c8f46b051e2910c011bc82bd78aad001410
rahul-practice-account <rahuldabhade1254@gmail.com>
2024-09-17 20:57:50 +0530

    Resolve the provider issue

--- main.tf
+++ main.tf
@@ -1,20 +1,13 @@
-
 provider "aws" {
-region                   = "us-east-1"
-shared_credentials_files = ["~/.aws/credentials"]
+  region                   = "us-east-1"
 }
-
 
 resource "aws_instance" "my-us-east-1-instance" {
-ami           = "ami-066784287e358dad1"
-instance_type = "t2.nano"
-availability_zone = "us-east-1a"
+  ami           = "ami-066784287e358dad1"
+  instance_type = "t2.nano"
+  availability_zone = "us-east-1a"
 
-tags = {
-  Name = "Rahul east-1 ec1 with zone"
-
+  tags = {
+    Name = "Rahul east-1 ec1 with zone"
+  }
 }
-
-}
-
- 
