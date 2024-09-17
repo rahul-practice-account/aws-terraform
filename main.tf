bf3fe01967c5b221bf11a37098be9ae8c7c218f1
rahul-practice-account <rahuldabhade1254@gmail.com>
2024-09-17 20:32:26 +0530

    Resolve the provider issue

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-terraform {
-  backend "s3" {
-    bucket         = "s306022024"
-    #key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
-
+# 
 provider "aws" {
   region = var.aws_region
 }
