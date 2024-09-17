ed3262665517770cc9eeaec044382ac5b525986c
rahul-practice-account <rahuldabhade1254@gmail.com>
2024-09-17 20:12:00 +0530

    Resolve the provider issue

--- main.tf
+++ main.tf
@@ -1,14 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
 
-  required_version = ">= 1.2.0"
+provider "aws" {
+  #region = "your-region" # Replace with your AWS region
 }
-
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0e86e20dae9224db8"
