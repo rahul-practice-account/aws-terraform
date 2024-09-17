a55c4088f08b4a3074e182757ea753ab3a947476
rahul-practice-account <rahuldabhade1254@gmail.com>
2024-09-17 20:54:05 +0530

    Resolve the provider issue

--- main.tf
+++ main.tf
@@ -1,36 +1,20 @@
-# 
+
 provider "aws" {
-  region = var.aws_region
+region                   = "us-east-1"
+shared_credentials_files = ["~/.aws/credentials"]
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
+resource "aws_instance" "my-us-east-1-instance" {
+ami           = "ami-066784287e358dad1"
+instance_type = "t2.nano"
+availability_zone = "us-east-1a"
+
+tags = {
+  Name = "Rahul east-1 ec1 with zone"
+
 }
 
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  tags = {
-    Name = var.name
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum -y update
-              sudo yum -y install httpd
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              EOF
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
+
+ 
